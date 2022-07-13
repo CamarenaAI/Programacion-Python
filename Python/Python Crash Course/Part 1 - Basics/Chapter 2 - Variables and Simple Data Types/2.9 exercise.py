eb4ff23b0211743fcf30948e66f1beefccae71ae
Alonso Ismael Camarena <71803835+IsmaelCamna@users.noreply.github.com>
2022-07-13 17:31:17 -0500

    Update 2.9 exercise.py

--- Python/Python Crash Course/Part 1 - Basics/Chapter 2 - Variables and Simple Data Types/2.9 exercise.py
+++ Python/Python Crash Course/Part 1 - Basics/Chapter 2 - Variables and Simple Data Types/2.9 exercise.py
@@ -1,7 +1,10 @@
-from email import message
+"""
+2.9 Favorite Number
 
-
-# 2.9 Favorite Number
+Store your favorite number in a variable. Then, using 
+that variable, create a message that reveals your favorite number. Print that 
+message
+"""
 favorite_number = 2
 message = "Your favorite number is: " + str(favorite_number)
 print(message)
