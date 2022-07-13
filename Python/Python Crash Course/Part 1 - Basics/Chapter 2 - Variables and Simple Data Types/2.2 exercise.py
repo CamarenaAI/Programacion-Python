bac191eb7b4e26905ac659fc770bc1393423346e
Alonso Ismael Camarena <71803835+IsmaelCamna@users.noreply.github.com>
2022-07-13 17:16:22 -0500

    Update 2.2 exercise.py

--- Python/Python Crash Course/Part 1 - Basics/Chapter 2 - Variables and Simple Data Types/2.2 exercise.py
+++ Python/Python Crash Course/Part 1 - Basics/Chapter 2 - Variables and Simple Data Types/2.2 exercise.py
@@ -1,4 +1,10 @@
-# 2.2 Simple Messages
+"""
+2.2 Simple Messages
+
+Store a message in a variable, and print that message.
+Then change the value of your variable to a new message, and print the new 
+message
+"""
 message = "Hello Pythoner, Welcome back!"
 print(message)
 
