3cf9458c5e2e049bbaff5f8222ef3ca00488eb98
Alonso Ismael Camarena <71803835+IsmaelCamna@users.noreply.github.com>
2022-07-13 17:50:12 -0500

    Update 3.2 exercise.py

--- Python/Python Crash Course/Part 1 - Basics/Chapter 3 - Introducing List/3.2 exercise.py
+++ Python/Python Crash Course/Part 1 - Basics/Chapter 3 - Introducing List/3.2 exercise.py
@@ -1,4 +1,11 @@
-# 3.2 Grettings
+"""
+3.2 Grettings
+
+Start with the list you used in Exercise 3-1, but instead of just 
+printing each person’s name, print a message to them. The text of each message
+should be the same, but each message should be personalized with the 
+person’s name
+"""
 names = ['Andrea', 'Tony', 'Juan R', 'Juan L', 'Laritza', 'German', 'Isabel']
 print(names[0] + " is very smart")
 print(names[1] + " work in a Starup")
