159c6e543d11d3190ad1ecfff041fba211f9e955
Alonso Ismael Camarena <71803835+IsmaelCamna@users.noreply.github.com>
2022-07-13 18:02:52 -0500

    Update 3.4 exercise.py

--- Python/Python Crash Course/Part 1 - Basics/Chapter 3 - Introducing List/3.4 exercise.py
+++ Python/Python Crash Course/Part 1 - Basics/Chapter 3 - Introducing List/3.4 exercise.py
@@ -1,4 +1,11 @@
-# 3.4 Guest List
+"""
+3.4 Guest List
+
+If you could invite anyone, living or deceased, to dinner, who 
+would you invite? Make a list that includes at least three people you’d like to 
+invite to dinner. Then use your list to print a message to each person, inviting 
+them to dinner
+"""
 guests = ['Bill Gates', 'Marck Zuckerberg', 'Jeff Bezos']
 
 message_1 = "Hello Mr." + guests[0] + " \nI have a dinner in my house the next week, \nI would like you to come\n"
