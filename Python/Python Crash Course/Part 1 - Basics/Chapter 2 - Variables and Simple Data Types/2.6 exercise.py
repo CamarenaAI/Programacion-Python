b124f4b1e57091947e9fb4e6834cb9236eab1d9a
Alonso Ismael Camarena <71803835+IsmaelCamna@users.noreply.github.com>
2022-07-13 17:26:55 -0500

    Update 2.6 exercise.py

--- Python/Python Crash Course/Part 1 - Basics/Chapter 2 - Variables and Simple Data Types/2.6 exercise.py
+++ Python/Python Crash Course/Part 1 - Basics/Chapter 2 - Variables and Simple Data Types/2.6 exercise.py
@@ -1,7 +1,10 @@
-from email import message
+"""
+2.6 Famous Quote 2
 
-
-# 2.6 Famous Quote 2
+Repeat Exercise 2-5, but this time store the famous person’s
+name in a variable called famous_person. Then compose your message 
+and store it in a new variable called message. Print your message
+"""
 famous_person = "Nikola Tesla "
 message = "I don't stole my idea... \n I care that they don't have any of their own."
 print(famous_person + "once said: " + message)
