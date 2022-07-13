34c20525c105a4a419139a3c5ff74d6063f7a011
Alonso Ismael Camarena <71803835+IsmaelCamna@users.noreply.github.com>
2022-07-13 17:28:31 -0500

    Update 2.7 exercise.py

--- Python/Python Crash Course/Part 1 - Basics/Chapter 2 - Variables and Simple Data Types/2.7 exercise.py
+++ Python/Python Crash Course/Part 1 - Basics/Chapter 2 - Variables and Simple Data Types/2.7 exercise.py
@@ -1,4 +1,13 @@
-# 2.7 Stripping Names
+"""
+2.7 Stripping Names
+
+Store a person’s name, and include some whitespace 
+characters at the beginning and end of the name. Make sure you use each 
+character combination, "\t" and "\n", at least once.
+    Print the name once, so the whitespace around the name is displayed.
+    Then print the name using each of the three stripping functions, 
+    lstrip(), rstrip(), and strip()
+"""
 name = "\tEric\n Erika"
 print(name.lstrip())
 
