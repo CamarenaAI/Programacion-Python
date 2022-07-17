73ff58529e73dd08df0e1b72bb563e90c962475a
IsmaelCamna <alonsocz200@hotmail.com>
2022-07-16 20:20:28 -0500

    Create list_4

---
+++ Books/Python Crash Course/Part I - Basics/Chapter 4 - Working with List/4.- list_4.py
@@ -0,0 +1,51 @@
+# Working with Part of a List
+
+# Slicing a List
+players = ['charles', 'martina', 'michael', 'florence', 'eli'] 
+print(players[0:3])
+
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+print(players[1:4])
+
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+print(players[:4])
+
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+print(players[2:])
+
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+print(players[-3:])
+
+# Looping Through a Slice
+players = ['charles', 'martina', 'michael', 'florence', 'eli'] 
+
+print("Here are the first three players on my team:")
+for player in players[:3]:
+    print(player.title())
+
+# Copying a List
+my_foods = ['pizza', 'falafel', 'carrot cake']
+friend_foods = my_foods[:]
+print("My favorite foods are:")
+print(my_foods)
+print("\nMy friend's favorite foods are:")
+print(friend_foods,"\n")
+
+my_foods = ['pizza', 'falafel', 'carrot cake'] 
+friend_foods = my_foods[:]
+my_foods.append('cannoli')
+friend_foods.append('ice cream')
+print("My favorite foods are:")
+print(my_foods)
+print("\nMy friend's favorite foods are:")
+print(friend_foods,"\n")
+
+my_foods = ['pizza', 'falafel', 'carrot cake']
+# This doesn't work:
+friend_foods = my_foods
+my_foods.append('cannoli')
+friend_foods.append('ice cream')
+print("My favorite foods are:")
+print(my_foods)
+print("\nMy friend's favorite foods are:")
+print(friend_foods)
