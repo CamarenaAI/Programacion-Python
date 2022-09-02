9fba90934bc8dbf1891562eb1abde85625f61f5d
IsmaelCamna <alonsocz200@hotmail.com>
2022-09-01 20:53:42 -0500

    Updated 5.- If Statement

--- Books/Python Crash Course/Part I - Basics/Chapter 5 - If Statements/5.- if_2.py
+++ Books/Python Crash Course/Part I - Basics/Chapter 5 - If Statements/5.- if_2.py
@@ -53,7 +53,37 @@ else:
 # Using and to Check Multiple Conditions
 age_0 = 22 
 age_1 = 22
-if age_0 >= 21 and age_1 >= 21:
+if age_0 >= 21 and age_1 >= 21: # It is necessary to meet the 2 conditions
     print(True)
 else:
     print(False)
+
+# Using or to Check Multiple Conditions
+age_0 = 10 
+age_1 = 22
+if age_0 >= 21 or age_1 >= 21: # It is necessary to meet 1 condition
+    print(True)
+else:
+    print(False)
+
+# Checking Whether a Value Is in a List
+requested_toppings = ['mushrooms', 'onions', 'pineapple']
+'mushrooms' in requested_toppings
+print(True)
+'pepperoni' in requested_toppings
+print(False)
+
+# Checking Whether a Value Is Not in a List
+banned_users = ['andrew', 'carolina', 'david']
+user = 'marie'
+if user not in banned_users:
+    print(user.title() + ", you can post a response if you wish.")
+
+# Boolean Expressions
+"""
+A Boolean expression is just another name for a 
+conditional test. A Boolean value is either True or False, just like the value 
+of a conditional expression after it has been evaluated
+"""
+game_active = True
+can_edit = False
