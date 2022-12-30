4cd5ff3f73e45cd541c0e0ee82d250e0f1fa37ed
Alonso Ismael Camarena <71803835+CamarenaAI@users.noreply.github.com>
2022-12-30 15:56:20 -0600

    Create 5.3 Exercise.py

---
+++ Books/Python Crash Course/Part I - Basics/Chapter 5 - If Statements/Excercises/5.3 Exercise.py
@@ -0,0 +1,22 @@
+"""
+5.3 Alien Colors #1
+
+Imagine an alien was just shot down in a game. Create a
+variable called alien_color and assign it a value of 'green', 'yellow', or 'red'
+
+•    Write an if statement to test whether the alien's color is green. If it is, print
+a message that the player just earned 5 points 
+•    Write one version of this program that passes the if test and another that
+fails. (The version that fails will have no output.)
+"""
+# Alien Color 5.3.1
+alien_color = 'green' # Assign it a value of 'green', 'yellow', or 'red'
+if 'green' in alien_color:
+    print("Congratulations, You earn 5 points")
+
+# Alien Color 5.3.2
+alien_color = 'white' # Assign it a value distinct of 'green', 'yellow', or 'red'
+if 'green' in alien_color:
+    print("Congratulations, You earn 5 points")
+else:
+    print("Sorry, You don't earn points")
