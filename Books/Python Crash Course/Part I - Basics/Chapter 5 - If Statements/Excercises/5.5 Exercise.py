d5da331467ad699d25ce5bfe9bcbe66c985603a2
Alonso Ismael Camarena <71803835+CamarenaAI@users.noreply.github.com>
2022-12-30 16:22:34 -0600

    Create 5.5 Exercise.py

---
+++ Books/Python Crash Course/Part I - Basics/Chapter 5 - If Statements/Excercises/5.5 Exercise.py
@@ -0,0 +1,22 @@
+"""
+5.5 Alien Colors #3
+
+Turn your if-else chain from Exercise 5.4 into if-elif-
+else chain 
+
+•    If the alien's color is green, print a message that the player earned 5 points
+•    If the alien's color is yellow, print a message that the player earned 10 points
+•    If the alien's color is red, print a message that the player earned 15 points
+•    Write three versions of this program, making sure each message is printed
+for the appropriate color alien
+"""
+
+alien_color = 'green'
+if alien_color == 'green':
+    print("Congratulations, You earn 5 points")
+elif alien_color == 'yellow':
+    print("Congratulations, You earn 10 points")
+elif alien_color == 'red':
+    print("Congratulations, You earn 15 points")
+else:
+    print("Sorry, You don't earn points")
