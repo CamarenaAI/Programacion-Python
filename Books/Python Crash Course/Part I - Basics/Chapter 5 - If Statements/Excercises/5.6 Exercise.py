ac08d2bddbb60d6548fbfe36092a5bb47889193b
Alonso Ismael Camarena <71803835+CamarenaAI@users.noreply.github.com>
2023-01-02 10:56:15 -0600

    Update 5.6 Exercise.py

--- Books/Python Crash Course/Part I - Basics/Chapter 5 - If Statements/Excercises/5.6 Exercise.py
+++ Books/Python Crash Course/Part I - Basics/Chapter 5 - If Statements/Excercises/5.6 Exercise.py
@@ -20,14 +20,16 @@ elder
 age = 24
 
 if age < 2:
-    print("Is a baby")
+    age = "baby"
 elif age >= 2 and age < 4:
-    print("Is a tolddler")
+    age = "toodler"
 elif age >= 4 and age < 13:
-    print("Is a kid")
+    age = "kid"
 elif age >= 13 and age < 20:
-    print("Is a teenager")
+    age = "teenager"
 elif age >= 20 and age < 65:
-    print("Is a adult")
+    age = "adult"
 else:
-    print("Is an elder")
+    age = "elder"
+    
+print("The person is a/an " + age)
