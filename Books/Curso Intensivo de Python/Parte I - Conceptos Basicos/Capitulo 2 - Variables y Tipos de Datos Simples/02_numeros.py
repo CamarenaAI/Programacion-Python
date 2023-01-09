eda609225afbc91fa2c4a8c77cb59cfba7d2e85b
Alonso Ismael Camarena <71803835+CamarenaAI@users.noreply.github.com>
2023-01-09 12:01:59 -0600

    Update and rename 2.- numbers.py to 02_numeros.py

---
+++ Books/Curso Intensivo de Python/Parte I - Conceptos Basicos/Capitulo 2 - Variables y Tipos de Datos Simples/02_numeros.py
@@ -0,0 +1,32 @@
+# Numeros
+
+# Enteros
+# Puedes sumar (+), restar (-), multiplicar (*), y dividir (/) numeros enteros en Python
+print(2 + 3)
+print(3 - 2)
+print(2 * 3)
+print(3 / 2)
+
+# Python usa el simbolo de multiplicacion dos veces para representar exponentes(**)
+print(3 ** 2)
+print(3 ** 3)
+print(10 ** 6)
+
+# Python tambien soporta el orden de operaciones, puedes usar multiples operaciones en una expresion
+print(2 + 3 * 4)
+print((2 + 3) * 4)
+
+# Flotantes
+print(0.1 + 0.1)
+print(0.2 + 0.2) 
+print(2 * 0.1) 
+print(2 * 0.2 )
+
+# Tenga en cuenta que a veces puede obtener un número arbitrario de lugares decimales en su respuesta
+print(0.2 + 0.1)
+print(3 * 0.1)
+
+# Evitar errores de tipo con la función str()
+edad = 23
+mensaje = "Feliz " + str(age) + "ro cumpleaños" # Puede hacer esto envolviendo la variable en la función str(), que le dice a Python que represente valores que no son cadenas como cadenas.
+print(mensaje)
