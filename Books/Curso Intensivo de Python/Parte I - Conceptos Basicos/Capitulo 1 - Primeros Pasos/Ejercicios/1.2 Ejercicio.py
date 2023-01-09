7cd5c6ffe8a6a2d612b96da126bf4f355b2f29d5
Alonso Ismael Camarena <71803835+CamarenaAI@users.noreply.github.com>
2023-01-09 11:46:55 -0600

    Update 1.2 Ejercicio.py

--- Books/Curso Intensivo de Python/Parte I - Conceptos Basicos/Capitulo 1 - Primeros Pasos/Ejercicios/1.2 Ejercicio.py
+++ Books/Curso Intensivo de Python/Parte I - Conceptos Basicos/Capitulo 1 - Primeros Pasos/Ejercicios/1.2 Ejercicio.py
@@ -1,10 +1,10 @@
 """
-1.2 Hello World Typos
+1.2 Errores tipograficos de Hola Mundo
 
-Open the hello_world.py file you just created. 
-Make a  typo somewhere in the line and run the program again. 
-Can you make a typo  that generates an error? 
-Can you make sense of the error message? 
-Can you  make a typo that doesn’t generate an error? 
-Why do you think it didn’t make  an error? 
+Abra el archivo hello_world.py que acaba de crear.
+Haga un error tipográfico en algún lugar de la línea y vuelva a ejecutar el programa.
+¿Se puede hacer un error tipográfico que genera un error?
+¿Puedes entender el mensaje de error?
+¿Puedes hacer un error tipográfico que no genere un error?
+¿Por qué crees que no cometió un error?
 """
