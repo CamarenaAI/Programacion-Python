eb3e66c91975eb2710092dafbf171d09cc0e7915
Alonso Ismael Camarena <71803835+CamarenaAI@users.noreply.github.com>
2023-01-09 11:27:03 -0600

    Create 5.- if_5.py

---
+++ Books/Python Crash Course/Part I - Basics/Chapter 5 - If Statements/5.- if_5.py
@@ -0,0 +1,7 @@
+# Styling Your if Statements
+
+# The recommendation PEP 8 provides for styling conditional test is to use a single space around comparison operators, such as ==, >=, <=
+if age < 4:
+  
+# Is better than:
+if age<4:
