7897d4cc47753e92100a021a53fcb5cc0ce3044c
Alonso Ismael Camarena <71803835+CamarenaAI@users.noreply.github.com>
2023-01-09 12:31:24 -0600

    Update and rename 2.- variables.py to 02_variables.py

---
+++ Books/Curso Intensivo de Python/Parte I - Conceptos Basicos/Capitulo 2 - Variables y Tipos de Datos Simples/02_variables.py
@@ -0,0 +1,19 @@
+# Variables
+
+mensaje = "¡Hola mundo Python!"
+print(mensaje)
+
+mensaje = "¡Hola mundo del Curso Intensivo de Python!"
+print(mensaje)
+
+# Evite errores de nombre al usar variables
+"""
+mensaje = "¡Hola, lector del Curso Intensivo de Python!"
+print(mensage)
+Output = SyntaxError: invalid syntax
+
+Please change mensaje for mensage
+"""
+
+mensaje = "¡Hola, lector del Curso Intensivo de Python!"
+print(mensaje)
