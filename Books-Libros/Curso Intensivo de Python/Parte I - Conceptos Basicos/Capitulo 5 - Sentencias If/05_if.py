ec965a656f6f39b52ec8f40fdbce35e5eee6b0b6
CamarenaAI <alonsocz200@hotmail.com>
2023-03-14 15:44:30 -0600

    5.- Sentencias If

--- Books-Libros/Curso Intensivo de Python/Parte I - Conceptos Basicos/Capitulo 5 - Sentencias If/05_if.py
+++ Books-Libros/Curso Intensivo de Python/Parte I - Conceptos Basicos/Capitulo 5 - Sentencias If/05_if.py
@@ -1,9 +1,11 @@
 # Diseñar sus declaraciones if
 
-# La recomendación que proporciona PEP 8 para la prueba condicional de
-# estilo es usar un solo espacio alrededor de los operadores de
-# comparación, como ==, >=, <=if age < 4:
+"""
+La recomendación que proporciona PEP 8 para la prueba condicional de
+estilo es usar un solo espacio alrededor de los operadores de
+comparación, como ==, >=, <=if age < 4:
 
+"""
 age = 23
 if age < 4:
     print(age)
