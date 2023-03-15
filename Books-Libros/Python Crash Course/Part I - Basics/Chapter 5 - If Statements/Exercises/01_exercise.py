5fbc77714660e55dc675ae1dcb1dd1e58df468d3
CamarenaAI <alonsocz200@hotmail.com>
2023-03-15 13:02:10 -0600

    4.- Working with List

--- Books-Libros/Python Crash Course/Part I - Basics/Chapter 5 - If Statements/Exercises/01_exercise.py
+++ Books-Libros/Python Crash Course/Part I - Basics/Chapter 5 - If Statements/Exercises/01_exercise.py
@@ -1,6 +1,6 @@
 """
 5.1 Conditional Tests
-
+                                                                       |
 Write a series of conditional tests. Print a statement 
 describing each test and your prediction for the results of each test. Your code 
 should look something like this:
