50348f3a5cf444c526599e0f297ad5cfddf2da14
CamarenaAI <alonsocz200@hotmail.com>
2023-03-15 11:50:02 -0600

    2.- Variables and Simple Data Types

--- Books-Libros/Python Crash Course/Part I - Basics/Chapter 2 - Variables and Simple Data Types/Exercises/01_exercise.py
+++ Books-Libros/Python Crash Course/Part I - Basics/Chapter 2 - Variables and Simple Data Types/Exercises/01_exercise.py
@@ -1,8 +1,8 @@
 """
 2.1 Simple Message
-                                                                       |
-Store a message in a variable, and then print that 
-message
+
+Store a message in a variable, and then print that message
 """
+
 message = "Hello would you like learn Python today?"
 print(message)
--- Books-Libros/Python Crash Course/Part I - Basics/Chapter 2 - Variables and Simple Data Types/Exercises/02_exercise.py
+++ Books-Libros/Python Crash Course/Part I - Basics/Chapter 2 - Variables and Simple Data Types/Exercises/02_exercise.py
@@ -1,10 +1,10 @@
 """
 2.2 Simple Messages
 
-Store a message in a variable, and print that message.
-Then change the value of your variable to a new message, and print the new 
-message
+Store a message in a variable, and print that message. Then change the
+value of your variable to a new message, and print the new message
 """
+
 message = "Hello Pythoner, Welcome back!"
 print(message)
 
