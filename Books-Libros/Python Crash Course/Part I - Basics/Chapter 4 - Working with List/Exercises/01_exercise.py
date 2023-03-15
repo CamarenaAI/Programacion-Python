ac3bbaf19368347589a0a3dafdcb10c0ad7d444f
CamarenaAI <alonsocz200@hotmail.com>
2023-03-15 12:08:28 -0600

    3.- Introducing List

--- Books-Libros/Python Crash Course/Part I - Basics/Chapter 4 - Working with List/Exercises/01_exercise.py
+++ Books-Libros/Python Crash Course/Part I - Basics/Chapter 4 - Working with List/Exercises/01_exercise.py
@@ -1,6 +1,6 @@
 """
 4.1 Pizzas
-
+                                                                       |
 Think of at least three kinds of your favorite pizza. Store these 
 pizza names in a list, and then use a for loop to print the name of each pizza
 •   Modify your for loop to print a sentence using the name of the pizza 
