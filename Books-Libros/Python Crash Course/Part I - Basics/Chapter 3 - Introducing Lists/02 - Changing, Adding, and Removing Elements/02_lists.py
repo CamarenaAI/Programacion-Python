3c7fca6080a79ef5c7f214057f63952f32a37540
camarenaai <alonsocz200@hotmail.com>
2023-04-17 10:41:33 -0600

    3.- Introducing Lists

--- Books-Libros/Python Crash Course/Part I - Basics/Chapter 3 - Introducing Lists/02 - Changing, Adding, and Removing Elements/02_lists.py
+++ Books-Libros/Python Crash Course/Part I - Basics/Chapter 3 - Introducing Lists/02 - Changing, Adding, and Removing Elements/02_lists.py
@@ -3,11 +3,17 @@
 motorcycles = ['honda', 'yamaha', 'suzuki']
 print(motorcycles)
 
-motorcycles.append('ducati') # Appending Elements to the End of a List
+# Appending Elements to the End of a List
+motorcycles.append('ducati')
 print(motorcycles)
 
 motorcycles = [] 
 motorcycles.append('honda') 
 motorcycles.append('yamaha') 
 motorcycles.append('suzuki') 
+print(motorcycles)
+
+# Inserting Elements into a List
+motorcycles = ['honda', 'yamaha', 'suzuki'] 
+motorcycles.insert(0, 'ducati') # You can add a new element at any position in your list by using the insert() method 
 print(motorcycles)
--- Books-Libros/Python Crash Course/Part I - Basics/Chapter 3 - Introducing Lists/02 - Changing, Adding, and Removing Elements/03_lists.py
+++ Books-Libros/Python Crash Course/Part I - Basics/Chapter 3 - Introducing Lists/02 - Changing, Adding, and Removing Elements/03_lists.py
@@ -1,5 +1,44 @@
-# Inserting Elements into a List
+# Removing Elements from a List
 
 motorcycles = ['honda', 'yamaha', 'suzuki'] 
-motorcycles.insert(0, 'ducati') # You can add a new element at any position in your list by using the insert() method 
 print(motorcycles)
+
+# Removing an Item Using the del Statement
+del motorcycles[0]
+print(motorcycles)
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+del motorcycles[1] 
+print(motorcycles)
+
+# Removing an Item Using the pop() Method
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+
+popped_motorcycle = motorcycles.pop() # The pop() method removes the last item in a list, but it lets you work with that item after removing it
+print(motorcycles)
+print(popped_motorcycle)
+
+motorcycles = ['honda', 'yamaha', 'suzuki'] 
+last_owned = motorcycles.pop() 
+print("The last motorcycle I owned was a " + last_owned.title() + ".")
+
+# popping items from any position in a List
+first_owned = motorcycles.pop(0) # You can actually use pop() to remove an item in a list at any position by including the index of the item you want to remove in parentheses
+print('The first motorcycle I owned was a ' + first_owned.title() + '.')
+
+# Removing an Item by Value
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati'] 
+print(motorcycles)
+
+motorcycles.remove('ducati') # If you only know the value of the item you want to remove, you can use the remove() method
+print(motorcycles)
+
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati'] 
+print(motorcycles)
+
+too_expensive = 'ducati'
+motorcycles.remove(too_expensive) # You can also use the remove() method to work with a value that’s being removed from a list
+print(motorcycles)
+print("\nA " + too_expensive.title() + " is too expensive for me.")
