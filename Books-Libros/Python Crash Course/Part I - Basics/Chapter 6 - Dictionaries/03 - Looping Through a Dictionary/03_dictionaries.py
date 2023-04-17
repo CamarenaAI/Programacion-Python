1e80571e02dae9c343bb78f1be3cca803d2b5095
camarenaai <alonsocz200@hotmail.com>
2023-04-17 11:21:49 -0600

    6.- Dictionaries

---
+++ Books-Libros/Python Crash Course/Part I - Basics/Chapter 6 - Dictionaries/03 - Looping Through a Dictionary/03_dictionaries.py
@@ -0,0 +1,11 @@
+# Looping Through a Dictionary's Keys in Order
+
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python',
+}
+
+for name in sorted(favorite_languages.keys()):
+    print(name.title() + ", thank you for taking the poll.")
--- Books-Libros/Python Crash Course/Part I - Basics/Chapter 6 - Dictionaries/04 - Nesting/01_dictionaries.py
+++ Books-Libros/Python Crash Course/Part I - Basics/Chapter 6 - Dictionaries/04 - Nesting/01_dictionaries.py
@@ -1,6 +1,5 @@
-# Nesting
-
 # A List of Dictionaries
+
 alien_0 = {'color': 'green', 'points': 5}
 alien_1 = {'color': 'yellow', 'points': 10}
 alien_2 = {'color': 'red', 'points': 15}
@@ -64,51 +63,3 @@ for alien in aliens[0:3]:
 for alien in aliens[:5]:
     print(alien)
 print("...")
-
-# A List in a Dictionary
-# Store information about a pizza being ordered
-pizza = {
-    'crust': 'thick',
-    'toppings': ['mushroom', 'extra cheese']
-}
-
-# Sumarizzae the order
-print("You ordered a " + pizza['crust'] + "-crust pizza " +
-    "with the following toppings:")
-
-for topping in pizza['toppings']:
-    print("\t" + topping)
-
-favorite_languages = {
-    'jen': ['python', 'ruby'],
-    'sarah': ['c'],
-    'edward': ['ruby', 'go'],
-    'phil': ['python', 'haskell']
-}
-
-for name, languages in favorite_languages.items():
-    print("\n" + name.title() + "'s favorite languages are:")
-    for language in languages:
-        print("\t" + language.title())
-
-# A Dictionary in a Dictionary
-users = {
-    'aeinstein': {
-        'first': 'albert',
-        'last': 'einstein',
-        'location': 'princeton',
-        },
-    
-    'mcurie': {
-        'first': 'marie',
-        'last': 'curie',
-        'location': 'paris',
-        },
-    }
-
-for username, user_info in users.items():
-    print("\nUsername: " + username)
-    full_name = user_info['first'] + " " + user_info['last']
-    location = user_info['location']
-    print("\tFull name: " + full_name.title())
-    print("\tLocation: " + location.title())
