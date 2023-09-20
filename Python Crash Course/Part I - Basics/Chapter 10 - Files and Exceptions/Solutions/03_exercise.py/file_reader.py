12b32613967c554be41514b9b13e772d02a0b780
camarenaai <alonsocz200@hotmail.com>
2023-09-20 15:02:41 -0600

    Chapter 10 - Files and Exceptions

---
+++ Python Crash Course/Part I - Basics/Chapter 10 - Files and Exceptions/Solutions/03_exercise.py/file_reader.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+path = Path('./Files/pi_digits.txt')
+contents = path.read_text()
+
+lines = contents.splitlines()
+for line in lines:
+    print(line)
+
+for line in contents.splitlines():
+    print(line)
---
+++ Python Crash Course/Part I - Basics/Chapter 10 - Files and Exceptions/Solutions/03_exercise.py/pi_string.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+path = Path('./Files/pi_digits.txt')
+contents = path.read_text()
+
+lines = contents.splitlines()
+pi_string = ''
+for line in lines:
+    pi_string += line.lstrip()
+
+print(f"{pi_string[:52]}...")
+print(len(pi_string))
+
+pi_string = ''
+for line in contents.splitlines():
+    pi_string += line.lstrip()
+
+print(f"{pi_string[:52]}...")
+print(len(pi_string))
